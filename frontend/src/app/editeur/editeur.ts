0106d3e520b626d494cae90df0baecdf2ae4b045
rasim.erben <rasim.erben@etu.umontpellier.fr>
2025-11-19 09:17:49 +0100

    Initialisation de editeur pour affichage

---
+++ frontend/src/app/editeur/editeur.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editeur',
+  imports: [],
+  templateUrl: './editeur.html',
+  styleUrl: './editeur.scss'
+})
+export class Editeur {
+
+}
